3d71993e2531845191b3c069629bcfd89d2ccfba
Ali Mirmohammad <ali.mirmohammad.86@gmail.com>
2022-05-12 20:40:12 +0430

    initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import MarketingApp from "./components/MarketingApp";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Container</h1>
+      <hr />
+      <MarketingApp />
+    </div>
+  );
+}
---
+++ packages/container/src/components/MarketingApp.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useRef } from "react";
+
+import { mount } from "marketing/MarketingApp";
+
+export default function MarketingApp() {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    mount(ref.current);
+  }, []);
+
+  return <div ref={ref} />;
+}
